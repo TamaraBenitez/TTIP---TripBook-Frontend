c3bec34f21a7fe8d2d022922375d3e8f1cc7b3e4
franarci <fran.arciniega96@gmail.com>
2024-09-01 18:30:55 -0300

    toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Trips from "./components/TripList/Trips";
 import Header from "./components/Header/Header";
+import { Toolbar } from "@mui/material";
 
 export default function App() {
-  return (<>
-  <Header />
-    <Routes>
-      <Route path="/" element={Home()}/>
-      <Route path="/trips" element={Trips()} />
-    </Routes>
-  </>)
+  
+
+  return (
+    <>
+        <Header />
+        <Toolbar sx={{height: "70px"}}/>
+        <Routes>
+          <Route path="/" element={Home()} />
+          <Route path="/trips" element={Trips()} />
+        </Routes>
+    </>
+  );
 }
