c7525b15ab357b59d5458b5b7b181a23cbc7543f
franarci <fran.arciniega96@gmail.com>
2024-09-01 18:30:35 -0300

    store context

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { CssBaseline } from '@mui/material';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './App.jsx'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './index.css';
+import React, { StrictMode, useContext } from "react";
+import { createRoot } from "react-dom/client";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Axios } from "axios";
+import TripService from "./services/TripService.jsx";
+import App from "./App.jsx";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.css";
+import StoreContext from "./store/storecontext.jsx";
 
-createRoot(document.getElementById('root')).render(
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#226668",
+    },
+    secondary: {
+      main: "#4E8487",
+    },
+  },
+});
+
+const store = {
+  services:{
+    tripService: new TripService(Axios,''),
+  }
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <CssBaseline />
-      <App />
-    </Router>
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <StoreContext.Provider value={store}>
+        <Router>
+          <CssBaseline />
+          <App />
+        </Router>
+      </StoreContext.Provider>
+    </ThemeProvider>
+  </StrictMode>
+);
