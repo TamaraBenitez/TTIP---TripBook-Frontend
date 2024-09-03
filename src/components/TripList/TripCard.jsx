082ca499683569051612a71165e2493b58eb654b
franarci <fran.arciniega96@gmail.com>
2024-09-03 16:04:49 -0300

    added 'unirme' button

--- src/components/TripList/TripCard.jsx
+++ src/components/TripList/TripCard.jsx
@@ -1,6 +1,9 @@
 import {
   Box,
+  Button,
   Card,
+  CardActionArea,
+  CardActions,
   CardContent,
   CardMedia,
   Container,
@@ -21,20 +24,21 @@ export default function TripCard({
 }) {
   const [isHovered, setIsHovered] = useState(false);
 
+  const handleClick = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <>
       <Card
         variant="outlined"
         className="tripCard"
-        component={NavLink}
-        to={`/trips/${to}`}
         sx={{
           textDecoration: "none",
           width: 345,
           transition: "transform 0.3s ease-in-out",
           "&:hover": {
             transform: "scale(1.05)",
-            cursor: "pointer",
           },
           position: "relative",
         }}
@@ -42,76 +46,85 @@ export default function TripCard({
         onMouseLeave={() => setIsHovered(false)}
         onClick={() => handleClick}
       >
-        <CardMedia
-          sx={{ height: 140, position: "relative" }}
-          image="/images/road.jpg"
-          title="green iguana"
-        >
-          {isHovered && (
-            <Box
-              sx={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "30%",
-                bgcolor: "rgba(255, 255, 255, 0.7)",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Container sx={{ display: "flex", justifyContent: "center" }}>
-                <LocationOn />
-                <Typography variant="h6">{destination}</Typography>
-              </Container>
-            </Box>
-          )}
-        </CardMedia>
-        <CardContent
-          sx={{
-            maxHeight: 300,
-            overflow: "hidden",
-          }}
-        >
-          <Container sx={{ display: "flex", paddingLeft: "0px !important" }}>
-            <Typography variant="h5" component="div">
-              {name}
-            </Typography>
+        <CardActionArea component={NavLink} to={`/trips/${to}`}>
+          <CardMedia
+            sx={{ height: 140, position: "relative" }}
+            image="/images/road.jpg"
+            title="green iguana"
+          >
             {isHovered && (
-              <Container
+              <Box
                 sx={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "30%",
+                  bgcolor: "rgba(255, 255, 255, 0.7)",
                   display: "flex",
-                  paddingRight: "0px !important",
-                  width: "20%",
-                  marginRight: "0px",
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
               >
-                <Groups className="groupIcon" />
-                {participantsNumber}
-              </Container>
+                <Container sx={{ display: "flex", justifyContent: "center" }}>
+                  <LocationOn />
+                  <Typography variant="h6">{destination}</Typography>
+                </Container>
+              </Box>
             )}
-          </Container>
+          </CardMedia>
+          <CardContent
+            sx={{
+              maxHeight: 300,
+              overflow: "hidden",
+            }}
+          >
+            <Container sx={{ display: "flex", paddingLeft: "0px !important" }}>
+              <Typography variant="h5" component="div">
+                {name}
+              </Typography>
+              {isHovered && (
+                <Container
+                  sx={{
+                    display: "flex",
+                    paddingRight: "0px !important",
+                    width: "20%",
+                    marginRight: "0px",
+                  }}
+                >
+                  <Groups className="groupIcon" />
+                  {participantsNumber}
+                </Container>
+              )}
+            </Container>
 
-          <Typography>{startDate}</Typography>
+            <Typography>{startDate}</Typography>
 
-          {isHovered && (
-            <Typography
-              className={isHovered ? "less-width" : ""}
-              variant="body2"
-              sx={{
-                color: "text.secondary",
-                display: "-webkit-box", 
-                WebkitLineClamp: 3, 
-                WebkitBoxOrient: "vertical",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {description}
-            </Typography>
-          )}
-        </CardContent>
+            {isHovered && (
+              <Typography
+                className={isHovered ? "less-width" : ""}
+                variant="body2"
+                sx={{
+                  color: "text.secondary",
+                  display: "-webkit-box",
+                  WebkitLineClamp: 3,
+                  WebkitBoxOrient: "vertical",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                {description}
+              </Typography>
+            )}
+          </CardContent>
+        </CardActionArea>
+        {isHovered && (
+          <CardActions>
+            <Button size="small" color="primary" onClick={handleClick}>
+              Unirme
+            </Button>
+          </CardActions>
+        )}
       </Card>
     </>
   );
