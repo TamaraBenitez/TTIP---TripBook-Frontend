988f8c9b7ad640079d483c095c2a6c9766b800e2
franarci <fran.arciniega96@gmail.com>
2024-09-09 13:48:02 -0300

    my trips

--- src/components/MyTrips/MyTrips.jsx
+++ src/components/MyTrips/MyTrips.jsx
@@ -1,27 +1,42 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Trips from '../TripList/Trips'
-import { Typography } from '@mui/material';
+import { Card, CardContent, Skeleton, Typography } from '@mui/material';
 import StoreContext from '../../store/storecontext';
 
 export default function MyTrips() {
     const [trips, setTrips] = useState([]);
+    const [loading, setLoading] = useState(true);
     const store = useContext(StoreContext);
     console.log("mytrips")
 useEffect(()=>{
-    const mytrips = store.services.tripService.GetMyTrips()
-        // .then((res)=>{
-        //     setTrips(res.data);
-        // })
-        // .catch((e)=>{
-        //     console.log(e)
-        // })
-    setTrips(mytrips)
-},[])
+    store.services.userService.GetMyTrips()
+        .then((res)=>{
+            setTrips(res.data);
+            setLoading(false);
+        })
+        .catch((e)=>{
+            console.log(e);
+            setLoading(false);
+        })
+},[]);
 
   return (
     <>
       <Typography variant="h4">Mis Viajes</Typography>
+      {loading ? (
+        // Show 5 Skeleton cards while loading
+        [...Array(5)].map((_, index) => (
+          <Card key={index} style={{ marginBottom: "1rem" }}>
+            <Skeleton variant="rectangular" height={140} />
+            <CardContent>
+              <Skeleton variant="text" />
+              <Skeleton variant="text" width="60%" />
+            </CardContent>
+          </Card>
+        ))
+      ) : (
       <Trips trips={trips} action={"detalles"} />
+      )}
     </>
   );
 }
