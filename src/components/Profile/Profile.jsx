eeb8920ab3e9306b6b5850f5d95bdfaf3558df89
franarci <fran.arciniega96@gmail.com>
2024-10-02 09:45:31 -0300

    not verified alert

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useContext } from "react";
-import { jwtDecode } from "jwt-decode";
 import StoreContext from "../../store/storecontext";
 import {
   Avatar,
@@ -8,46 +7,34 @@ import {
   Grid,
   CircularProgress,
   Tooltip,
+  Button,
+  Alert,
 } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
+import { useUser } from "../../user/UserContext";
 
 const Profile = () => {
   const [userInfo, setUserInfo] = useState(null);
   const { services } = useContext(StoreContext);
   const [loading, setLoading] = useState(true);
-
-  const getInitials = (name, surname) => {
-    return `${name.charAt(0).toUpperCase()}${surname.charAt(0).toUpperCase()}`;
-  };
+  const { user } = useUser();
+  
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
+      const userId = user.id;
 
-    if (token) {
-      try {
-        const decoded = jwtDecode(token);
-        console.log("Contenido del token:", decoded);
-
-        const userId = decoded.id;
-
-        services.userService
-          .GetUser(userId)
-          .then((response) => {
-            console.log("User data:", response.data);
-            setUserInfo(response.data);
-            setLoading(false);
-          })
-          .catch((error) => {
-            console.error("Error fetching user data:", error);
-          });
-      } catch (error) {
-        console.error("Error decoding token:", error);
-      }
-    } else {
-      console.error("No token found in localStorage");
-    }
-  }, [services]);
+      services.userService
+        .GetUser(userId)
+        .then((response) => {
+          console.log("User data:", response.data);
+          setUserInfo(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+        });
+  }, [user, services]);
 
   if (loading) {
     return <CircularProgress />;
@@ -56,38 +43,50 @@ const Profile = () => {
   if (!userInfo) {
     return <Typography>Error loading profile</Typography>;
   }
+  
+  const getInitials = (name, surname) => {
+    return `${name.charAt(0).toUpperCase()}${surname.charAt(0).toUpperCase()}`;
+  };
 
   const handleNull = (value) => value || "Sin información aun";
 
+  const handleVerify = () => {
+    services.userService.verifyUser(userInfo.id)
+      .then(() => {
+        console.log('User verified successfully');
+      })
+      .catch((error) => {
+        console.error('Error verifying user:', error);
+      });
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start", // Posiciona a la izquierda
-        marginLeft: 5,
-      }}
-    >
-      {/* Avatar, Nombre y Validación en la misma fila */}
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start", marginLeft: 5, marginBottom:5 }}>
+      {!userInfo.isUserVerified && (
+        <Alert variant="outlined" severity="warning" sx={{alignSelf: "center", width:"20vw"}}>
+        Account not verified
+        <Typography variant="body1" color="textPrimary">
+            Your account is not verified.
+          </Typography>
+          <Button variant="outlined" color="primary" onClick={handleVerify} sx={{ mt: 1 }}>
+            Verify Now
+          </Button>
+      </Alert>
+      )}
+
       <Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
         <Avatar sx={{ bgcolor: "#226668", width: 70, height: 70, mr: 2 }}>
           {getInitials(userInfo.name, userInfo.surname)}
         </Avatar>
         <Typography variant="h4" sx={{ display: "flex", alignItems: "center" }}>
           {userInfo.name} {userInfo.surname}{" "}
-          {userInfo.isValidated ? (
+          {userInfo.isUserVerified ? (
             <Tooltip title="Validado">
-              <CheckCircleIcon
-                color="success"
-                sx={{ ml: 1 }} // Margen entre texto e ícono
-              />
+              <CheckCircleIcon color="success" sx={{ ml: 1 }} />
             </Tooltip>
           ) : (
             <Tooltip title="No validado">
-              <CancelIcon
-                color="error"
-                sx={{ ml: 1 }} // Margen entre texto e ícono
-              />
+              <CancelIcon color="error" sx={{ ml: 1 }} />
             </Tooltip>
           )}
         </Typography>
