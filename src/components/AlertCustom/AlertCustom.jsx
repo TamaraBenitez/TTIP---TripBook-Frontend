f89d6ec86082bcb8c1c276afb8893a8bcb342c00
TamaraBenitez01 <tamara.benitez@kuru.la>
2024-10-20 23:25:57 -0300

    fix modal register

--- src/components/AlertCustom/AlertCustom.jsx
+++ src/components/AlertCustom/AlertCustom.jsx
@@ -1,24 +1,33 @@
-import { TaskAlt } from '@mui/icons-material'
-import { Alert, Fade } from '@mui/material'
-import React from 'react'
+import { TaskAlt } from "@mui/icons-material";
+import { Alert, Fade } from "@mui/material";
+import React from "react";
 
-export default function AlertCustom({inProp, timeout, onClose, msg, icon, severity}) {
+export default function AlertCustom({
+  inProp,
+  timeout,
+  onClose,
+  msg,
+  icon,
+  severity,
+}) {
   return (
     <Fade in={inProp} timeout={timeout}>
-          <Alert
-            sx={{
-              maxWidth: 500,
-              position: "fixed",
-              top: "130px",
-              right: "10px",
-            }}
-            onClose={onClose}
-            variant="standard"
-            icon={icon ? icon : <TaskAlt fontSize="inherit" />}
-            severity={severity}
-          >
-            {msg}
-          </Alert>
-        </Fade>
-  )
+      <Alert
+        sx={{
+          maxWidth: 500,
+          position: "fixed",
+          bottom: "30px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: 2,
+        }}
+        onClose={onClose}
+        variant="standard"
+        icon={icon ? icon : <TaskAlt fontSize="inherit" />}
+        severity={severity}
+      >
+        {msg}
+      </Alert>
+    </Fade>
+  );
 }
