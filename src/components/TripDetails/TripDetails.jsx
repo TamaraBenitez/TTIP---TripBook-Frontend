a057f4baa1eab2cdf4621c6f438b592b45e2b0e4
franarci <fran.arciniega96@gmail.com>
2024-11-02 15:21:33 -0300

    prettier

--- src/components/TripDetails/TripDetails.jsx
+++ src/components/TripDetails/TripDetails.jsx
@@ -29,7 +29,7 @@ export default function TripDetails() {
   const [open, setOpen] = useState(false);
   const [isRegistered, setIsRegistered] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const { user, setUser } = useUser();
+  const { user } = useUser();
   const navigate = useNavigate();
   const handleClickOpen = (e) => {
     e.stopPropagation();
@@ -43,26 +43,18 @@ export default function TripDetails() {
   const handleSuscribe = (e) => {
     setLoading(true);
     handleCloseModal();
-    navigate(`/trip/suscribe/${id}`)
-    // store.services.tripService.RegisterUserToTrip(user.id, id)
-    // .then((res) => {
-    //   setLoading(false);
-    //   setIsRegistered(true);
-    //   setShowAlert(true);
-    // });
+    navigate(`/trip/suscribe/${id}`);
   };
 
   useEffect(() => {
     store.services.tripService
       .GetTrip(id)
       .then((res) => {
-        debugger;
         setTrip(res.data);
         const alreadyRegistered = res.data.participants.find(
           (p) => p.id == user.id
         );
-        if(alreadyRegistered)
-        setIsRegistered(true);
+        if (alreadyRegistered) setIsRegistered(true);
         setLoading(false);
       })
       .catch((e) => {
@@ -73,124 +65,138 @@ export default function TripDetails() {
 
   return (
     <>
-    <Box sx={{display:"flex", flexDirection: "row", alignItems:"center"}}>
-
-      <Card sx={{ maxWidth: 600, margin: "20px auto", padding: "20px" }}>
-        <CardContent>
-          {loading ? (
-            <>
-              <Skeleton variant="text" height={40} width="60%" />
-              <Skeleton variant="text" width="40%" />
-              <Skeleton variant="text" />
-              <Skeleton variant="text" />
-              <Skeleton variant="rectangular" height={118} />
-            </>
-          ) : (
-            <>
-              <Typography variant="h4" component="h1" gutterBottom>
-                Viaje a {trip.destination}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                <strong>Fecha de salida:</strong> {formatDate(trip.startDate, true)}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                <strong>Desde:</strong> {trip.origin}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                <strong>Descripción:</strong> {trip.description}
-              </Typography>
-              <Typography variant="body1" color="textSecondary">
-                <strong>Costo estimado:</strong> ${trip.estimatedCost}
-              </Typography>
-
-              <Divider sx={{ margin: "20px 0" }} />
-
-              <Typography variant="h5" component="h2" gutterBottom>
-                Participantes
-              </Typography>
-              {trip.participants?.length > 0 ? (
-                trip.participants.map((user) => (
-                  <Accordion key={user.id} sx={{ maxWidth: 250 }}>
-                    <AccordionSummary
-                      sx={{ justifyContent: "flex-start" }}
-                      aria-controls="panel1-content"
-                      id="panel1-header"
-                      >
-                      <ListItemIcon sx={{ minWidth: 40 }}>
-                        <AccountCircle />
-                      </ListItemIcon>
-                      <Typography variant="body1">
-                        {user.name} {user.surname}
-                      </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails
-                      sx={{ WebkitLineClamp: 3, WebkitBoxOrient: "vertical" }}
-                      >
-                      <Box>
-                        <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          sx={{
-                            overflow: "hidden",
-                            textOverflow: "ellipsis",
-                          }}
-                          >
-                          Email: {user.email}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          sx={{
-                            overflow: "hidden",
-                            textOverflow: "ellipsis",
-                          }}
-                          >
-                          Localidad: {user.locality}, {user.province}
-                        </Typography>
-                      </Box>
-                    </AccordionDetails>
-                  </Accordion>
-                ))
-              ) : (
-                <Typography variant="body1">
-                  No hay participantes registrados aún.
+      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+        <Card sx={{ maxWidth: 600, margin: "20px auto", padding: "20px" }}>
+          <CardContent>
+            {loading ? (
+              <>
+                <Skeleton variant="text" height={40} width="60%" />
+                <Skeleton variant="text" width="40%" />
+                <Skeleton variant="text" />
+                <Skeleton variant="text" />
+                <Skeleton variant="rectangular" height={118} />
+              </>
+            ) : (
+              <>
+                <Typography variant="h4" component="h1" gutterBottom>
+                  Viaje a {trip.destination}
                 </Typography>
-              )}
-              {!isRegistered && (
-                <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: "20px",
-                }}
-                >
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleClickOpen}
+                <Typography variant="body1" color="textSecondary">
+                  <strong>Fecha de salida:</strong>{" "}
+                  {formatDate(trip.startDate, true)}
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  <strong>Desde:</strong> {trip.origin}
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  <strong>Descripción:</strong> {trip.description}
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  <strong>Costo estimado:</strong> ${trip.estimatedCost}
+                </Typography>
+
+                <Divider sx={{ margin: "20px 0" }} />
+
+                <Typography variant="h5" component="h2" gutterBottom>
+                  Participantes
+                </Typography>
+                {trip.participants?.length > 0 ? (
+                  trip.participants.map((user) => (
+                    <Accordion key={user.id} sx={{ maxWidth: 250 }}>
+                      <AccordionSummary
+                        sx={{ justifyContent: "flex-start" }}
+                        aria-controls="panel1-content"
+                        id="panel1-header"
+                      >
+                        <ListItemIcon sx={{ minWidth: 40 }}>
+                          <AccountCircle />
+                        </ListItemIcon>
+                        <Typography variant="body1">
+                          {user.name} {user.surname}
+                        </Typography>
+                      </AccordionSummary>
+                      <AccordionDetails
+                        sx={{ WebkitLineClamp: 3, WebkitBoxOrient: "vertical" }}
+                      >
+                        <Box>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            sx={{
+                              overflow: "hidden",
+                              textOverflow: "ellipsis",
+                            }}
+                          >
+                            Email: {user.email}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            sx={{
+                              overflow: "hidden",
+                              textOverflow: "ellipsis",
+                            }}
+                          >
+                            Localidad: {user.locality}, {user.province}
+                          </Typography>
+                        </Box>
+                      </AccordionDetails>
+                    </Accordion>
+                  ))
+                ) : (
+                  <Typography variant="body1">
+                    No hay participantes registrados aún.
+                  </Typography>
+                )}
+                {!isRegistered && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      marginTop: "20px",
+                    }}
+                  >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleClickOpen}
                     >
-                    Unirme al viaje
-                  </Button>
-                </Box>
-              )}
-            </>
-          )}
-        </CardContent>
-      </Card>
-      {trip.tripCoordinates && <MapComponent coordinates={trip.tripCoordinates[0]}/>}
-      <DialogCustom
-        open={open}
-        handleClose={handleCloseModal}
-        handleConfirm={handleSuscribe}
-        title={"Inscripcion al viaje"}
-        textParagraph={"¿Esta seguro que desea inscribirse a este viaje?"}
-        showCancelButton={true}
+                      Unirme al viaje
+                    </Button>
+                  </Box>
+                )}
+              </>
+            )}
+          </CardContent>
+        </Card>
+        {trip.tripCoordinates && (
+          <MapComponent coordinates={trip.tripCoordinates[0]} />
+        )}
+        <DialogCustom
+          open={open}
+          handleClose={handleCloseModal}
+          handleConfirm={handleSuscribe}
+          title={"Inscripcion al viaje"}
+          textParagraph={"¿Esta seguro que desea inscribirse a este viaje?"}
+          showCancelButton={true}
         />
-      {showAlert &&
-        <Alert sx={{maxWidth:500, position:"fixed", left:600, bottom:300, zIndex:3}}onClose={()=>setShowAlert(false)} variant="outlined" icon={<TaskAlt fontSize="inherit" />} severity="success">
-          Te has Inscripto exitosamente a este viaje
-        </Alert>
-      }
+        {showAlert && (
+          <Alert
+            sx={{
+              maxWidth: 500,
+              position: "fixed",
+              left: 600,
+              bottom: 300,
+              zIndex: 3,
+            }}
+            onClose={() => setShowAlert(false)}
+            variant="outlined"
+            icon={<TaskAlt fontSize="inherit" />}
+            severity="success"
+          >
+            Te has Inscripto exitosamente a este viaje
+          </Alert>
+        )}
       </Box>
     </>
   );
--- src/components/TripRegistration/TripRegistration.jsx
+++ src/components/TripRegistration/TripRegistration.jsx
@@ -6,7 +6,6 @@ import { useUser } from "../../user/UserContext";
 import { Box, CircularProgress } from "@mui/material";
 import DialogCustom from "../DialogCustom/DialogCustom";
 
-
 export default function TripRegistration() {
   const [trip, setTrip] = useState({ tripCoordinates: null });
   const { id } = useParams();
@@ -14,39 +13,41 @@ export default function TripRegistration() {
   const store = useContext(StoreContext);
   const [departureCoords, setDepartureCoords] = useState(null);
   const [open, setOpen] = useState(false);
-  const [modalTitle, setModalTitle] = useState("Propuesta enviada")
-  const [modalMsg, setModalMsg] = useState("Hemos notificado al conductor.")
+  const [modalTitle, setModalTitle] = useState("Propuesta enviada");
+  const [modalMsg, setModalMsg] = useState("Hemos notificado al conductor.");
   const navigate = useNavigate();
   const proposeNewRoute = (pickPoint) => {
     let requestBody = {
       userId: user.id,
       tripId: trip.id,
       latitude: pickPoint[0],
-      longitude: pickPoint[1]
-    } 
-    store.services.tripService.RegisterUserToTrip(requestBody)
-    .then(()=>{
-      setOpen(true)
-    })
-    .catch((error)=>{
-      debugger;
-      setModalTitle("Error")
-      setModalMsg(error.response.data.message);
-      setOpen(true)
-    })
-  }
+      longitude: pickPoint[1],
+    };
+    store.services.tripService
+      .RegisterUserToTrip(requestBody)
+      .then(() => {
+        setOpen(true);
+      })
+      .catch((error) => {
+        setModalTitle("Error");
+        setModalMsg(error.response.data.message);
+        setOpen(true);
+      });
+  };
 
   useEffect(() => {
     if (!trip.tripCoordinates) {
       store.services.tripService
-      .GetTrip(id)
-      .then((res) => {
+        .GetTrip(id)
+        .then((res) => {
           setTrip(res.data);
-          setDepartureCoords([res.data.tripCoordinates[0].latitude, res.data.tripCoordinates[0].longitude]);
-          const alreadyRegistered = res.data.participants.find(
-            (p) => {
-              return p.id == user.id}
-          );
+          setDepartureCoords([
+            res.data.tripCoordinates[0].latitude,
+            res.data.tripCoordinates[0].longitude,
+          ]);
+          const alreadyRegistered = res.data.participants.find((p) => {
+            return p.id == user.id;
+          });
           if (alreadyRegistered) setIsRegistered(true);
           setLoading(false);
         })
@@ -56,21 +57,28 @@ export default function TripRegistration() {
     }
   }, [trip]);
   return (
-    <>{userDataLoading ? (
-      <Box sx={{ display: "flex", justifyContent: "center", height: "100%" }}>
-        <CircularProgress />
-      </Box>
-    ) :
-      departureCoords && trip.tripCoordinates && ( 
-          <MapComponent isRegistering={true} proposeNewRoute={proposeNewRoute} coordinates={trip.tripCoordinates[0]}/>
+    <>
+      {userDataLoading ? (
+        <Box sx={{ display: "flex", justifyContent: "center", height: "100%" }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        departureCoords &&
+        trip.tripCoordinates && (
+          <MapComponent
+            isRegistering={true}
+            proposeNewRoute={proposeNewRoute}
+            coordinates={trip.tripCoordinates[0]}
+          />
+        )
       )}
 
       <DialogCustom
-            open={open}
-            handleConfirm={()=>navigate("/")}
-            handleClose={()=>navigate("/")}
-            title={modalTitle}
-            textParagraph={modalMsg}
+        open={open}
+        handleConfirm={() => navigate("/")}
+        handleClose={() => navigate("/")}
+        title={modalTitle}
+        textParagraph={modalMsg}
       />
     </>
   );
