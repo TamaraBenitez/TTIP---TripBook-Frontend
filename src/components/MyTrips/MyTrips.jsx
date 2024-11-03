33be78218975278408202363d1e6c9db02265296
TamaraBenitez01 <tamara.benitez@kuru.la>
2024-11-03 14:55:13 -0300

    add tabs my trips

--- src/components/MyTrips/MyTrips.jsx
+++ src/components/MyTrips/MyTrips.jsx
@@ -13,6 +13,12 @@ import StoreContext from "../../store/storecontext";
 import RibbonHeading from "../RibbonHeading/RibbonHeading";
 import { useUser } from "../../user/UserContext";
 import TripsSkeleton from "../TripList/TripsSkeleton";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItem";
 
 export default function MyTrips() {
   const [trips, setTrips] = useState([]);
@@ -20,23 +26,54 @@ export default function MyTrips() {
   const store = useContext(StoreContext);
   const { user, userDataLoading, setUser } = useUser();
   const navigate = useNavigate();
+  const [value, setValue] = useState(0);
+  const [pendingPassengers, setPendingPassengers] = useState([]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   useEffect(() => {
-    if(!userDataLoading){
-    store.services.userService
-      .GetMyTrips(user.id)
-      .then((res) => {
-        console.log("TRIPS LOADED");
-        setTrips(res.data);
-        setLoading(false);
-      })
-      .catch((e) => {
-        console.log(e);
-        setLoading(false);
-      });
+    if (!userDataLoading) {
+      setLoading(true);
+
+      if (value === 2) {
+        // Llama al endpoint de pasajeros pendientes
+        store.services.tripService
+          .GetPendingPassengers(user.id)
+          .then((res) => {
+            setPendingPassengers(res.data);
+            setLoading(false);
+          })
+          .catch((e) => {
+            console.log(e);
+            setLoading(false);
+          });
+      } else {
+        const role = value === 0 ? "passenger" : "driver";
+
+        store.services.userService
+          .GetMyTrips(user.id, role)
+          .then((res) => {
+            setTrips(res.data);
+            setLoading(false);
+          })
+          .catch((e) => {
+            console.log(e);
+            setLoading(false);
+          });
+      }
     }
-  }, [userDataLoading]);
+  }, [userDataLoading, value]);
+
   const redirectToTrip = (e, to) => {
     navigate(`/trips/${to}`);
+  };
+
+  const handleViewDetails = (tripUserId, tripId) => {
+    alert("hellouuuu");
+    console.log(tripUserId);
+    console.log(tripId);
   };
 
   return (
@@ -45,16 +82,118 @@ export default function MyTrips() {
         sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
       >
         <RibbonHeading heading={"Mis Viajes"} component="h2" variant="h2" />
+
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: "divider",
+            width: "50%",
+            mx: "auto",
+            mt: 2,
+          }}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Como Pasajero" />
+            <Tab label="Como Conductor" />
+            <Tab label="Solicitudes Pendientes" />
+          </Tabs>
+        </Box>
+
+        {/* Panel de contenido para cada pestaña */}
         {loading || userDataLoading ? (
           <TripsSkeleton />
         ) : (
-          <Trips
-            trips={trips}
-            action={"detalles"}
-            handleAction={redirectToTrip}
-          />
+          <>
+            <TabPanel value={value} index={0}>
+              <Trips
+                trips={trips}
+                action={"detalles"}
+                handleAction={redirectToTrip}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <Trips
+                trips={trips}
+                action={"detalles"}
+                handleAction={redirectToTrip}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              <List>
+                {pendingPassengers.map((trip) => (
+                  <Box
+                    key={trip.tripId}
+                    sx={{
+                      border: "1px solid #ccc",
+                      borderRadius: "8px",
+                      p: 2,
+                      mb: 2,
+                    }}
+                  >
+                    <Typography variant="h6" sx={{ color: "#226668" }}>
+                      {trip.origin} ➔ {trip.destination}
+                    </Typography>
+                    <List>
+                      {trip.pendingPassengers.map((passenger) => (
+                        <Box key={passenger.tripUserId} sx={{ ml: 2, mt: 1 }}>
+                          <Typography variant="body1" color="textSecondary">
+                            Solicita: {passenger.name} {passenger.surname}
+                          </Typography>
+                          <ListItemText
+                            primary={passenger.email}
+                            sx={{ ml: 2, mb: 1 }}
+                          />
+                          <Button
+                            variant="outlined"
+                            onClick={() =>
+                              handleViewDetails(
+                                passenger.tripUserId,
+                                trip.tripId
+                              )
+                            }
+                          >
+                            Ver más detalles de solicitud
+                          </Button>
+                        </Box>
+                      ))}
+                    </List>
+                  </Box>
+                ))}
+              </List>
+            </TabPanel>
+          </>
         )}
       </Box>
     </>
   );
 }
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  value: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+};
--- src/services/TripService.jsx
+++ src/services/TripService.jsx
@@ -1,43 +1,50 @@
-import dayjs from "dayjs";
 import BaseService from "./BaseService";
 class TripService extends BaseService {
-    constructor(axios, apiUrl){
-        super(axios, apiUrl);
-    }
-    
-    GetAllTrips() {
-        return this.axios({
-            url: `${this.baseUrl}/trip`,
-            method: "GET",
-            headers: this.config.headers
-        });
-    };
+  constructor(axios, apiUrl) {
+    super(axios, apiUrl);
+  }
 
-    GetTrip(id){
-        return this.axios({
-            url: `${this.baseUrl}/trip/${id}`,
-            method: "GET",
-            headers: this.config.headers
-        });
-    }
+  GetAllTrips() {
+    return this.axios({
+      url: `${this.baseUrl}/trip`,
+      method: "GET",
+      headers: this.config.headers,
+    });
+  }
 
-    RegisterUserToTrip(data) {
-        return this.axios({
-            url: `${this.baseUrl}/tripUser/createRegistrationWithOtherCoordinates`,
-            method: "POST",
-            headers: this.config.headers,
-            data: data
-        });
-    }
+  GetTrip(id) {
+    return this.axios({
+      url: `${this.baseUrl}/trip/${id}`,
+      method: "GET",
+      headers: this.config.headers,
+    });
+  }
 
-    CreateTrip(tripData){
-        return this.axios({
-            url: `${this.baseUrl}/trip`,
-            method: "POST",
-            headers: this.config.headers,
-            data:tripData
-        })
-    }
+  GetPendingPassengers(id) {
+    return this.axios({
+      url: `${this.baseUrl}/trip/driver/${id}/pendingPassengers`,
+      method: "GET",
+      headers: this.config.headers,
+    });
+  }
+
+  RegisterUserToTrip(data) {
+    return this.axios({
+      url: `${this.baseUrl}/tripUser/createRegistrationWithOtherCoordinates`,
+      method: "POST",
+      headers: this.config.headers,
+      data: data,
+    });
+  }
+
+  CreateTrip(tripData) {
+    return this.axios({
+      url: `${this.baseUrl}/trip`,
+      method: "POST",
+      headers: this.config.headers,
+      data: tripData,
+    });
+  }
 }
 
 export default TripService;
