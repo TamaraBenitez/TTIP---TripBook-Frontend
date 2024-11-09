0acbe5605a317b92feb32fd148af4d3c8bd339aa
TamaraBenitez01 <tamara.benitez@kuru.la>
2024-11-09 02:43:05 -0300

    fix import

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@src": "/src",
+    },
+  },
   optimizeDeps: {
     include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
   },
